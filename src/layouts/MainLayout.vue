e887fb963fb937ae60034504bc213aeb32e4fab9
CompileError <victor5517@gmail.com>
2023-03-22 22:51:30 +0800

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Simple Manufacturing Process Manager
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
+        <NavItem
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator';
+import NavItem from 'src/components/NavItem.vue';
+
+@Component({
+  components: {
+    NavItem,
+  },
+})
+export default class MainLayout extends Vue {
+  essentialLinks = [
+    {
+      title: 'Product Zipper',
+      caption: 'One click compress your product',
+      icon: 'folder_zip',
+      to: '/products',
+    },
+  ];
+
+  leftDrawerOpen = false;
+
+  toggleLeftDrawer() {
+    this.leftDrawerOpen = !this.leftDrawerOpen;
+  }
+}
+
+</script>
---
+++ src/modules/authentications/LoginPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="">
+    <q-layout view="lHh lpr lFf">
+      <q-header elevated>
+        <q-toolbar>
+          <q-toolbar-title>
+            Simple Manufacturing Process Manager
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+      <q-page-container>
+        <q-page padding class="row justify-center items-center">
+          <div class="column">
+            <div class="row">
+              <h5 class="text-h5 q-my-md">Login</h5>
+            </div>
+            <div class="row">
+              <q-card square bordered class="q-pa-lg shadow-1">
+                <q-card-section>
+                  <q-form class="q-gutter-md">
+                    <q-input ref="inputRef" square filled clearable v-model="userName"
+                    label="username"
+                    style="width: 300px;" />
+                    <q-input square filled clearable v-model="password" type="password"
+                    label="password" style="width: 300px;" />
+                  </q-form>
+                </q-card-section>
+                <q-card-actions class="q-px-md">
+                  <q-btn unelevated color="light-green-7" size="lg" class="full-width"
+                  label="Login" @click="onSubmit" />
+                </q-card-actions>
+              </q-card>
+            </div>
+          </div>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator';
+
+@Component({})
+export default class LoginPage extends Vue {
+  userName = '';
+
+  password = '';
+
+  async onSubmit(): Promise<void> {
+    this.$router.push('/');
+  }
+
+  onReset(): void {
+    this.userName = '';
+    this.password = '';
+  }
+}
+</script>
