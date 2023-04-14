dd4fc7d559f26dc8a02084d7bd40ddc7a9c0e8ec
JunHaoShih <victor5517@gmail.com>
2023-04-14 11:51:31 +0800

    fix: streamline part usage tree node generation

--- src/modules/partUsages/stores/PartUsageUsesStore.ts
+++ src/modules/partUsages/stores/PartUsageUsesStore.ts
@@ -1,13 +1,65 @@
 import { defineStore } from 'pinia';
-import { QTreeProps } from 'quasar';
+import { QTreeNode, QTreeProps } from 'quasar';
 import { PartVersion } from 'src/modules/parts/models/PartVersion';
 import { PartUsageUses } from '../models/PartUsageUses';
-import { PartUsageUsesTreeStore } from './PartUsageUsesTreeStore';
 
 export interface PartUsageUsesContainer {
   uses: Map<number, Map<number, PartUsageUses>>,
   root: PartVersion,
 }
+
+let idCounter = 0;
+
+const getSubTreeNodes = (mapValue: Map<number, PartUsageUses>, wholeMap: Map<number, Map<number, PartUsageUses>>): QTreeProps['nodes'] => {
+  const nodes = [] as QTreeNode[];
+  mapValue.forEach((value) => {
+    const currentNode: QTreeNode = {
+      id: idCounter,
+      label: `${value.uses.number} - ${value.uses.version.iteration}.${value.uses.version.revision}`,
+      icon: 'settings',
+      versionId: value.uses.version.id,
+      usageId: value.id,
+      lazy: true,
+    };
+    console.log(`Counter: ${idCounter}`);
+    idCounter += 1;
+    const children = wholeMap.get(value.uses.version.id);
+    if (children) {
+      currentNode.children = getSubTreeNodes(children, wholeMap);
+      currentNode.lazy = false;
+    }
+    nodes.push(currentNode);
+  });
+  return nodes;
+};
+
+const getTreeNodes = (uses: Map<number, Map<number, PartUsageUses>>, root: PartVersion): QTreeProps['nodes'] => {
+  idCounter = 0;
+  if (!root.master) {
+    return [{
+      id: idCounter,
+      label: '',
+      versionId: 0,
+      usageId: 0,
+      icon: 'settings',
+    }];
+  }
+  const rootNode: QTreeNode = {
+    id: idCounter,
+    label: `${root.master.number} - ${root.iteration}.${root.revision}`,
+    versionId: root.id,
+    usageId: 0,
+    icon: 'settings',
+  };
+  idCounter += 1;
+  if (root.id) {
+    const children = uses.get(root.id);
+    if (children) {
+      rootNode.children = getSubTreeNodes(children, uses);
+    }
+  }
+  return [rootNode];
+};
 
 export const PartUsageUsesStore = defineStore('partUsageUses', {
   state: (): PartUsageUsesContainer => ({
@@ -16,20 +68,21 @@ export const PartUsageUsesStore = defineStore('partUsageUses', {
   }),
   getters: {
     treeNodes: (state): QTreeProps['nodes'] => {
-      const partUsageUsesTreeStore = PartUsageUsesTreeStore();
       if (!state.root.master) {
         return [{
           label: '',
           icon: 'room_service',
         }];
       }
-      const rootNode = partUsageUsesTreeStore.getTreeNodes(state.uses, state.root);
-      return [rootNode];
+      console.log('Test');
+      const rootNode = getTreeNodes(state.uses, state.root);
+      return rootNode;
     },
   },
   actions: {
     initialize(usages: PartUsageUses[], partVersion: PartVersion) {
       this.root = partVersion;
+      this.uses = new Map<number, Map<number, PartUsageUses>>();
       for (let i = 0; i < usages.length; i += 1) {
         const usage = usages[i];
         if (!this.uses.has(usage.usedBy)) {
@@ -40,6 +93,17 @@ export const PartUsageUsesStore = defineStore('partUsageUses', {
         }
       }
       this.root = partVersion;
+    },
+    addUses(usages: PartUsageUses[]) {
+      for (let i = 0; i < usages.length; i += 1) {
+        const usage = usages[i];
+        if (!this.uses.has(usage.usedBy)) {
+          this.uses.set(usage.usedBy, new Map<number, PartUsageUses>());
+        }
+        if (!this.uses.get(usage.usedBy)?.has(usage.uses.version.id)) {
+          this.uses.get(usage.usedBy)?.set(usage.uses.version.id, usage);
+        }
+      }
     },
   },
 });
